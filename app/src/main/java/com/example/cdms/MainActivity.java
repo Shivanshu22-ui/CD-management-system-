ee1c2d51d9f0451e448c0da6a09db13c8c2d2467
yash-b1202 <yash.bhandari1202@gmail.com>
2022-02-15 00:59:13 +0530

    added buttons for navigating to different activities

--- app/src/main/java/com/example/cdms/MainActivity.java
+++ app/src/main/java/com/example/cdms/MainActivity.java
@@ -4,14 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button genbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent= new Intent(this,generate.class);
-        startActivity(intent);
+        genbtn=findViewById(R.id.generate);
+
+        genbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this,generate.class);
+                startActivity(intent);
+            }
+        });
     }
 }
