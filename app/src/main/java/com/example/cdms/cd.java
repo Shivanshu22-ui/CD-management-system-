ed4164f95c96a45da2fda896074c52e390eb46d4
yash-b1202 <yash.bhandari1202@gmail.com>
2022-02-16 11:50:51 +0530

    database for storing generated qr code done
    user login now directs straight to main activity for testing

---
+++ app/src/main/java/com/example/cdms/cd.java
@@ -0,0 +1,27 @@
+package com.example.cdms;
+
+public class cd {
+    private String name;
+    private String summary;
+
+    public cd(String name, String summary) {
+        this.name = name;
+        this.summary = summary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+}
