58334d8823e6de7ba75363b10778bf095079da2d
yash-b1202 <yash.bhandari1202@gmail.com>
2022-02-21 01:49:28 +0530

    minute changes v2.0

--- app/src/main/java/com/example/cdms/listOfcds.java
+++ app/src/main/java/com/example/cdms/listOfcds.java
@@ -22,26 +22,26 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-public class listOfBorrowers extends AppCompatActivity {
+public class listOfcds extends AppCompatActivity {
 ListView listView;
 FirebaseDatabase database;
 DatabaseReference ref;
 ArrayList <String> list;
 ArrayAdapter <String> adapter;
-Borrower borrower;
+cd cd;
 EditText inputSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_of_borrowers);
+        setContentView(R.layout.activity_list_of_cds);
 
-        borrower = new Borrower();
+        cd = new cd();
         listView=findViewById(R.id.listview);
         database = FirebaseDatabase.getInstance();
         ref = database.getReference("cd");
         list = new ArrayList<>();
-        adapter = new ArrayAdapter<String>(this,R.layout.activity_borrower_info, R.id.borrowerInfo,list);
+        adapter = new ArrayAdapter<String>(this,R.layout.activity_cd_info, R.id.borrowerInfo,list);
         inputSearch = (EditText) findViewById(R.id.inputSearch);
 
         ref.addValueEventListener(new ValueEventListener() {
@@ -49,8 +49,8 @@ EditText inputSearch;
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 for(DataSnapshot ds : snapshot.getChildren()){
 
-                    borrower = ds.getValue(Borrower.class);
-                    list.add(borrower.getName().toString() + "  -  " +borrower.getSummary().toString());
+                    cd = ds.getValue(cd.class);
+                    list.add(cd.getName().toString() + "  -  " +cd.getSummary().toString());
                 }
                 listView.setAdapter(adapter);
             }
@@ -64,9 +64,9 @@ EditText inputSearch;
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(listOfBorrowers.this, "Item clicked -"+adapter.getItem(i), Toast.LENGTH_SHORT).show();
+                Toast.makeText(listOfcds.this, "Item clicked -"+adapter.getItem(i), Toast.LENGTH_SHORT).show();
 
-                startActivity(new Intent(getApplicationContext(),BorrowerHistory.class));
+                startActivity(new Intent(getApplicationContext(), cdHistory.class));
             }
         });
 
@@ -75,7 +75,7 @@ EditText inputSearch;
             @Override
             public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
                 // When user changed the Text
-                listOfBorrowers.this.adapter.getFilter().filter(cs);
+                listOfcds.this.adapter.getFilter().filter(cs);
             }
 
             @Override
