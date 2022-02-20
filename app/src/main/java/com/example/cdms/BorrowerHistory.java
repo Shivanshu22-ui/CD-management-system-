0f8c0524490c0298054597059859aa45a4dd3d2d
Vidushi Singh <76548398+vidushi014@users.noreply.github.com>
2022-02-21 00:16:56 +0530

    Cd list and borrower list view

---
+++ app/src/main/java/com/example/cdms/BorrowerHistory.java
@@ -0,0 +1,14 @@
+package com.example.cdms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BorrowerHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_borrower_history);
+    }
+}
